af1781406afa940ce0c795f30c6bd467df931b2f
DK <darshankhot@programmer.net>
2020-11-12 14:57:50 +0530

    transformed login boxes to sub-components

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,10 +3,46 @@ import React from 'react';
 // bootstrap
 import {Form, Button} from 'react-bootstrap'
 
-const Home = () => {
-    const studentLogin = (e) => {
+const LoginBox = (props) => {
+    const userLogin = (e) => {
         e.preventDefault();
     }
+
+    return (
+        <div style={{
+            borderRadius: '5px',
+            padding: '30px',
+            margin: '10px',
+            color: '#B02A30',
+            backgroundColor: '#E7B909',
+            minWidth: '300px'
+        }}>
+            <h5>
+                {props.header}
+            </h5><hr/>
+            <Form onSubmit={userLogin}>
+                <Form.Group controlId="studentEmail">
+                    <Form.Label>VESIT Email ID</Form.Label>
+                    <Form.Control required type="email" placeholder={props.emailText} />
+                </Form.Group>
+
+                <Form.Group controlId="studentPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control required type="password" placeholder={props.passwordText} />
+                </Form.Group>
+                <Button block type="submit" style={{
+                    color: '#E7B909',
+                    backgroundColor: '#B02A30',
+                    border: 'none'
+                }}>
+                    Login
+                </Button>
+            </Form>
+        </div>
+    )
+}
+
+const Home = () => {
 
     const adminLogin = (e) => {
         e.preventDefault();
@@ -19,66 +55,8 @@ const Home = () => {
             justifyContent: 'space-evenly',
             alignItems: 'center'
         }}>
-            <div style={{
-                borderRadius: '5px',
-                padding: '30px',
-                margin: '10px',
-                color: '#B02A30',
-                backgroundColor: '#E7B909',
-                minWidth: '300px'
-            }}>
-                <h5>
-                    Student Login
-                </h5><hr/>
-                <Form onSubmit={studentLogin}>
-                    <Form.Group controlId="studentEmail">
-                        <Form.Label>VESIT Email address</Form.Label>
-                        <Form.Control required type="email" placeholder="Enter VESIT email" />
-                    </Form.Group>
-
-                    <Form.Group controlId="studentPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control required type="password" placeholder="Password given by college" />
-                    </Form.Group>
-                    <Button block type="submit" style={{
-                        color: '#E7B909',
-                        backgroundColor: '#B02A30',
-                        border: 'none'
-                    }}>
-                        Login
-                    </Button>
-                    </Form>
-            </div>
-            <div style={{
-                borderRadius: '5px',
-                padding: '30px',
-                margin: '10px',
-                color: '#B02A30',
-                backgroundColor: '#E7B909',
-                minWidth: '300px'
-            }}>
-                <h5>
-                    Office Login
-                </h5><hr/>
-                <Form onSubmit={adminLogin}>
-                    <Form.Group controlId="studentEmail">
-                        <Form.Label>VESIT Email address</Form.Label>
-                        <Form.Control required type="email" placeholder="Enter VESIT admin email" />
-                    </Form.Group>
-
-                    <Form.Group controlId="studentPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control required type="password" placeholder="Admin Password" />
-                    </Form.Group>
-                    <Button block type="submit" style={{
-                        color: '#E7B909',
-                        backgroundColor: '#B02A30',
-                        border: 'none'
-                    }}>
-                        Login
-                    </Button>
-                </Form>
-            </div>
+            <LoginBox header='Student Login' emailText='Enter your @ves.ac.in ID' passwordText='Enter provided password' />
+            <LoginBox header='Admin Login' emailText='Enter Admin ID' passwordText='Enter admin password' />
         </div>
     )
 }
