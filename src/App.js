4b657702807bee6c79746a13a3bf85a63f51dc6a
DK <darshankhot@programmer.net>
2020-11-13 22:17:56 +0530

    prettier extension changes in indentation

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import React from 'react';
-import {HashRouter as Router, Switch, Route} from 'react-router-dom';
+import React from "react";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 
 // Components
 // import Header from './components/Header';
-import Home from './components/Home';
-import StudentMain from './components/StudentMain'
-import StudentAddPost from './components/StudentAddPost'
-import Resolve from './components/StudentPostResolve'
-import AdminMain from './components/AdminMain'
-import AdminPostResolve from './components/AdminPostResolve'
+import Home from "./components/Home";
+import StudentMain from "./components/StudentMain";
+import StudentAddPost from "./components/StudentAddPost";
+import Resolve from "./components/StudentPostResolve";
+import AdminMain from "./components/AdminMain";
+import AdminPostResolve from "./components/AdminPostResolve";
 
 // Styling
-import './App.css'
+import "./App.css";
 // import {Container} from 'react-bootstrap'
-
 
 const App = () => {
   return (
@@ -21,18 +20,18 @@ const App = () => {
       {/* <Header></Header> */}
       <Router>
         <Switch>
-          <Route path='/' exact component={Home} />
+          <Route path="/" exact component={Home} />
           {/* student section */}
-          <Route path='/student' exact component={StudentMain} />
-          <Route path='/student/add-post' exact component={StudentAddPost} />
-          <Route path='/student/resolve' exact component={Resolve} />
+          <Route path="/student" exact component={StudentMain} />
+          <Route path="/student/add-post" exact component={StudentAddPost} />
+          <Route path="/student/resolve" exact component={Resolve} />
           {/* admin section */}
-          <Route path='/admin' exact component={AdminMain} />
-          <Route path='/admin/post' exact component={AdminPostResolve} />
+          <Route path="/admin" exact component={AdminMain} />
+          <Route path="/admin/post" exact component={AdminPostResolve} />
         </Switch>
       </Router>
     </div>
   );
-}
+};
 
 export default App;
